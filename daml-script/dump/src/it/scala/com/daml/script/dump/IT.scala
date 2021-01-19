953f9aa8e1bd4da9ec6c656fb1e44a05ef72f0b0
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-01-19 14:22:25 +0100

    .

--- daml-script/dump/src/it/scala/com/daml/script/dump/IT.scala
+++ daml-script/dump/src/it/scala/com/daml/script/dump/IT.scala
@@ -21,7 +21,11 @@ import com.daml.ledger.api.v1.ledger_offset.LedgerOffset
 import com.daml.ledger.api.v1.transaction_filter.{Filters, TransactionFilter}
 import com.daml.ledger.api.v1.{value => api}
 import com.daml.ledger.client.LedgerClient
-import com.daml.ledger.client.configuration.{CommandClientConfiguration, LedgerClientConfiguration, LedgerIdRequirement}
+import com.daml.ledger.client.configuration.{
+  CommandClientConfiguration,
+  LedgerClientConfiguration,
+  LedgerIdRequirement,
+}
 import com.daml.lf.archive.{Dar, DarReader, Decode}
 import com.daml.platform.sandbox.services.TestCommands
 import com.daml.platform.sandboxnext.SandboxNextFixture
@@ -56,126 +60,99 @@ final class IT
   private def iouId(s: String) =
     api.Identifier(packageId, moduleName = "Iou", s)
 
+  private def submit(client: LedgerClient, p: Ref.Party, cmd: Command) =
+    client.commandServiceClient.submitAndWaitForTransaction(
+      SubmitAndWaitRequest(
+        Some(
+          Commands(
+            ledgerId = client.ledgerId.unwrap,
+            applicationId = appId.unwrap,
+            commandId = UUID.randomUUID().toString(),
+            party = p,
+            commands = Seq(cmd),
+          )
+        )
+      )
+    )
+
   "Generated dump for IOU transfer compiles" in {
     for {
       client <- LedgerClient(channel, clientConfiguration)
       p1 <- client.partyManagementClient.allocateParty(None, None).map(_.party)
       p2 <- client.partyManagementClient.allocateParty(None, None).map(_.party)
-      t0 <- client.commandServiceClient.submitAndWaitForTransaction(
-        SubmitAndWaitRequest(
-          Some(
-            Commands(
-              ledgerId = client.ledgerId.unwrap,
-              applicationId = appId.unwrap,
-              commandId = UUID.randomUUID().toString(),
-              party = p1,
-              commands = Seq(
-                Command().withCreate(
-                  CreateCommand(
-                    templateId = Some(iouId("Iou")),
-                    createArguments = Some(
-                      api.Record(
-                        fields = Seq(
-                          api.RecordField("issuer", Some(api.Value().withParty(p1))),
-                          api.RecordField("owner", Some(api.Value().withParty(p1))),
-                          api.RecordField("currency", Some(api.Value().withText("USD"))),
-                          api.RecordField("amount", Some(api.Value().withNumeric("100"))),
-                          api.RecordField("observers", Some(api.Value().withList(api.List()))),
-                        )
-                      )
-                    ),
-                  )
+      t0 <- submit(
+        client,
+        p1,
+        Command().withCreate(
+          CreateCommand(
+            templateId = Some(iouId("Iou")),
+            createArguments = Some(
+              api.Record(
+                fields = Seq(
+                  api.RecordField("issuer", Some(api.Value().withParty(p1))),
+                  api.RecordField("owner", Some(api.Value().withParty(p1))),
+                  api.RecordField("currency", Some(api.Value().withText("USD"))),
+                  api.RecordField("amount", Some(api.Value().withNumeric("100"))),
+                  api.RecordField("observers", Some(api.Value().withList(api.List()))),
                 )
-              ),
-            )
+              )
+            ),
           )
-        )
+        ),
       )
       cid0 = t0.getTransaction.events(0).getCreated.contractId
-      t1 <- client.commandServiceClient.submitAndWaitForTransaction(
-        SubmitAndWaitRequest(
-          Some(
-            Commands(
-              ledgerId = client.ledgerId.unwrap,
-              applicationId = appId.unwrap,
-              commandId = UUID.randomUUID().toString(),
-              party = p1,
-              commands = Seq(
-                Command().withExercise(
-                  ExerciseCommand(
-                    templateId = Some(iouId("Iou")),
-                    choice = "Iou_Split",
-                    contractId = cid0,
-                    choiceArgument = Some(
-                      api
-                        .Value()
-                        .withRecord(
-                          api.Record(fields =
-                            Seq(api.RecordField(value = Some(api.Value().withNumeric("50"))))
-                          )
-                        )
-                    ),
+      t1 <- submit(
+        client,
+        p1,
+        Command().withExercise(
+          ExerciseCommand(
+            templateId = Some(iouId("Iou")),
+            choice = "Iou_Split",
+            contractId = cid0,
+            choiceArgument = Some(
+              api
+                .Value()
+                .withRecord(
+                  api.Record(fields =
+                    Seq(api.RecordField(value = Some(api.Value().withNumeric("50"))))
                   )
                 )
-              ),
-            )
+            ),
           )
-        )
+        ),
       )
       cid1 = t1.getTransaction.events(1).getCreated.contractId
       cid2 = t1.getTransaction.events(2).getCreated.contractId
-      t2 <- client.commandServiceClient.submitAndWaitForTransaction(
-        SubmitAndWaitRequest(
-          Some(
-            Commands(
-              ledgerId = client.ledgerId.unwrap,
-              applicationId = appId.unwrap,
-              commandId = UUID.randomUUID().toString(),
-              party = p1,
-              commands = Seq(
-                Command().withExercise(
-                  ExerciseCommand(
-                    templateId = Some(iouId("Iou")),
-                    choice = "Iou_Transfer",
-                    contractId = cid2,
-                    choiceArgument = Some(
-                      api
-                        .Value()
-                        .withRecord(
-                          api.Record(fields =
-                            Seq(api.RecordField(value = Some(api.Value().withParty(p2))))
-                          )
-                        )
-                    ),
-                  )
+      t2 <- submit(
+        client,
+        p1,
+        Command().withExercise(
+          ExerciseCommand(
+            templateId = Some(iouId("Iou")),
+            choice = "Iou_Transfer",
+            contractId = cid2,
+            choiceArgument = Some(
+              api
+                .Value()
+                .withRecord(
+                  api.Record(fields = Seq(api.RecordField(value = Some(api.Value().withParty(p2)))))
                 )
-              ),
-            )
+            ),
           )
-        )
+        ),
       )
       cid3 = t2.getTransaction.events(1).getCreated.contractId
-      _ <- client.commandServiceClient.submitAndWaitForTransaction(
-        SubmitAndWaitRequest(
-          Some(
-            Commands(
-              ledgerId = client.ledgerId.unwrap,
-              applicationId = appId.unwrap,
-              commandId = UUID.randomUUID().toString(),
-              party = p2,
-              commands = Seq(
-                Command().withExercise(
-                  ExerciseCommand(
-                    templateId = Some(iouId("IouTransfer")),
-                    choice = "IouTransfer_Accept",
-                    contractId = cid3,
-                    choiceArgument = Some(api.Value().withRecord(api.Record())),
-                  )
-                )
-              ),
-            )
+      _ <- submit(
+        client,
+        p2,
+        Command().withExercise(
+          ExerciseCommand(
+            templateId = Some(iouId("IouTransfer")),
+            choice = "IouTransfer_Accept",
+            contractId = cid3,
+            choiceArgument = Some(api.Value().withRecord(api.Record())),
           )
-        )
+        ),
       )
       _ <- Main.run(
         Config(
@@ -213,25 +190,36 @@ final class IT
           party_participants = Map.empty,
         ),
       )
-      transactions <- client.transactionClient.getTransactions(
-        LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_BEGIN),
-        Some(LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_END)),
-        transactionFilter(p3)).runWith(Sink.seq)
+      transactions <- client.transactionClient
+        .getTransactions(
+          LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_BEGIN),
+          Some(LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_END)),
+          transactionFilter(p3),
+        )
+        .runWith(Sink.seq)
       _ = transactions should have length 4
-      transactions <- client.transactionClient.getTransactions(
-        LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_BEGIN),
-        Some(LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_END)),
-        transactionFilter(p4)).runWith(Sink.seq)
+      transactions <- client.transactionClient
+        .getTransactions(
+          LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_BEGIN),
+          Some(LedgerOffset().withBoundary(LedgerOffset.LedgerBoundary.LEDGER_END)),
+          transactionFilter(p4),
+        )
+        .runWith(Sink.seq)
       _ = transactions should have length 2
-      acs <- client.activeContractSetClient.getActiveContracts(transactionFilter(p3)).runWith(Sink.seq)
+      acs <- client.activeContractSetClient
+        .getActiveContracts(transactionFilter(p3))
+        .runWith(Sink.seq)
       _ = acs.flatMap(_.activeContracts) should have size 2
       _ = println(acs)
-      acs <- client.activeContractSetClient.getActiveContracts(transactionFilter(p4)).runWith(Sink.seq)
+      acs <- client.activeContractSetClient
+        .getActiveContracts(transactionFilter(p4))
+        .runWith(Sink.seq)
       _ = acs.flatMap(_.activeContracts) should have size 1
       _ = println(acs)
       // TODO Validate the script result.
     } yield succeed
   }
 
-  private def transactionFilter(p: Ref.Party) = TransactionFilter(filtersByParty = Seq(p -> Filters()).toMap)
+  private def transactionFilter(p: Ref.Party) =
+    TransactionFilter(filtersByParty = Seq(p -> Filters()).toMap)
 }
