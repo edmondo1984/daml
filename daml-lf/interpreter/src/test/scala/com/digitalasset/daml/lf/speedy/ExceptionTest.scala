b90fdca1642fa6e316f6c918bd20ec7b9dc46662
Nick Chapman <nick.chapman@digitalasset.com>
2021-01-28 15:49:34 +0000

    improve organization of the exception tests

--- daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/ExceptionTest.scala
+++ daml-lf/interpreter/src/test/scala/com/digitalasset/daml/lf/speedy/ExceptionTest.scala
@@ -5,7 +5,6 @@ package com.daml.lf.speedy
 
 import com.daml.lf.PureCompiledPackages
 import com.daml.lf.data
-import com.daml.lf.language.Ast
 import com.daml.lf.language.Ast.{Expr, Package}
 import com.daml.lf.speedy.Compiler.FullStackTrace
 import com.daml.lf.speedy.SError._
@@ -18,8 +17,25 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class ExceptionTest extends AnyWordSpec with Matchers with TableDrivenPropertyChecks {
 
-  val pkg: Package =
-    p"""
+  "simple" should {
+
+    val pkgs: PureCompiledPackages = noPackages
+
+    val testCases = Table[String, Long](
+      ("expression", "expected"),
+      ("RUN_UPDATE @Int64 (upure @Int64 42)", 42),
+    )
+
+    forEvery(testCases) { (exp: String, expected: Long) =>
+      s"eval[$exp] --> $expected" in {
+        runExpr(pkgs)(e"$exp") shouldBe Right(SValue.SInt64(expected))
+      }
+    }
+  }
+
+  "throw-catch control flow" should {
+
+    val pkgs: PureCompiledPackages = typeAndCompile(p"""
        module M {
          val myThrow : forall (a: *). (Text -> a) =
            /\ (a: *). \(u : Text) ->
@@ -29,37 +45,31 @@ class ExceptionTest extends AnyWordSpec with Matchers with TableDrivenPropertyCh
            /\ (a: *). \ (handler: Text -> a) (body: Text -> a) ->
              RUN_UPDATE @a (try @a (upure @a (body "body-unitish")) catch e -> Some @(Update a) (upure @a (handler "handler-unitish")));
 
-         val func : (Int64 -> Int64) = \ (x: Int64) ->
+         val throwCatchTest : (Int64 -> Int64) = \ (x: Int64) ->
            (ADD_INT64 1000
             (M:myCatch @Int64 (\(u : Text) -> 100) (\(u : Text) ->
              ADD_INT64 10
               (case (EQUAL @Int64 (ADD_INT64 x 40) 42) of True -> M:myThrow @Int64 "throw-unitish" | False -> x))));
        }
-      """
+      """)
 
-  "RUN_UPDATE supplies token" in {
-    val exp = e"RUN_UPDATE @Int64 (upure @Int64 42)"
-    val expected = Right(SValue.SInt64(42))
-    runExpr(exp) shouldBe expected
-  }
+    val testCases = Table[String, Long](
+      ("expression", "expected"),
+      ("M:throwCatchTest 1", 1011),
+      ("M:throwCatchTest 2", 1100),
+      ("M:throwCatchTest 3", 1013),
+    )
 
-  "func(1) -- no exception" in {
-    val exp = e"M:func 1"
-    val expected = Right(SValue.SInt64(1011))
-    runExpr(exp) shouldBe expected
-  }
-  "func(2) -- exception thrown and caught" in {
-    val exp = e"M:func 2"
-    val expected = Right(SValue.SInt64(1100))
-    runExpr(exp) shouldBe expected
-  }
-  "func(3) -- no exception" in {
-    val exp = e"M:func 3"
-    val expected = Right(SValue.SInt64(1013))
-    runExpr(exp) shouldBe expected
+    forEvery(testCases) { (exp: String, expected: Long) =>
+      s"eval[$exp] --> $expected" in {
+        runExpr(pkgs)(e"$exp") shouldBe Right(SValue.SInt64(expected))
+      }
+    }
   }
 
-  private def typeAndCompile(pkg: Ast.Package): PureCompiledPackages = {
+  private def noPackages: PureCompiledPackages = PureCompiledPackages(Map.empty).toOption.get
+
+  private def typeAndCompile(pkg: Package): PureCompiledPackages = {
     val rawPkgs = Map(defaultParserParameters.defaultPackageId -> pkg)
     Validation.checkPackage(rawPkgs, defaultParserParameters.defaultPackageId, pkg)
     data.assertRight(
@@ -67,10 +77,8 @@ class ExceptionTest extends AnyWordSpec with Matchers with TableDrivenPropertyCh
     )
   }
 
-  val pkgs = typeAndCompile(pkg)
-
-  private def runExpr(e: Expr): Either[SError, SValue] = {
-    val machine = Speedy.Machine.fromPureExpr(pkgs, e)
+  private def runExpr(pkgs1: PureCompiledPackages)(e: Expr): Either[SError, SValue] = {
+    val machine = Speedy.Machine.fromPureExpr(pkgs1, e)
     machine.run() match {
       case SResultFinalValue(v) => Right(v)
       case SResultError(e) => Left(e)
@@ -78,6 +86,6 @@ class ExceptionTest extends AnyWordSpec with Matchers with TableDrivenPropertyCh
     }
   }
 
-  def crash[A](reason: String): A = throw new RuntimeException(reason)
+  private def crash[A](reason: String): A = throw new RuntimeException(reason)
 
 }
